0c8e4a4e565060ef6123c354ef22a4748949a5bc
Nathan Baldez <nathanbsb2@hotmail.com>
2023-07-23 20:28:45 -0300

    feat: port config

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = new ApolloServer({
 });
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: process.env.PORT || 4000 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
