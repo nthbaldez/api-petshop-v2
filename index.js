f1175a5a74c8238111bd18045531eb8d08a0977b
Nathan Baldez <nathanbsb2@hotmail.com>
2024-12-03 07:21:08 -0300

    fix: ajustar cors para a aplicação petshop do Johnny

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+// import { startStandaloneServer } from '@apollo/server/standalone';
+import express from 'express';
+import http from 'http';
 import {typeDefs } from './graphql/typeDefs.js';
 import { resolvers } from './graphql/resolvers.js';
+import cors from 'cors';
 
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: process.env.PORT || 4000 },
+// });
+
+const app = express();
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: process.env.PORT || 4000 },
-});
+await server.start();
 
-console.log(`🚀  Server ready at: ${url}`);
+app.use(
+  '/graphql',
+  cors({ origin: ['https://amigosdojohnny.vercel.app/', 'https://studio.apollographql.com'] }),
+  express.json(),
+  expressMiddleware(server),
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀  Server ready at: http://localhost:4000/graphql`);
